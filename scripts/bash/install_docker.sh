e24d136de90c5e3e9d6ebf7f483370feea886129
Joseph DiPilato <josephdipilato@Josephs-iMac.home>
2022-04-23 16:20:43 -0400

    docker mac

---
+++ scripts/bash/install_docker.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Mac
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install --cask docker
--- scripts/bash/local_setup.sh
+++ scripts/bash/local_setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "NOT IMPLEMENTED"
 echo "Installing Docker"
+./scripts/bash/install_docker.sh
