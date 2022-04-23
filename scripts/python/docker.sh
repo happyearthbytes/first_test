233074ba7a82ddbc896a0ad0925e064704958dc3
Joseph DiPilato <josephdipilato@Josephs-iMac.home>
2022-04-23 18:01:19 -0400

    docker run

--- scripts/python/docker.sh
+++ scripts/python/docker.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-# docker-compose --file environments/python/docker-compose.yml build
-docker-compose --file environments/python/docker-compose.yml up
+docker-compose --file environments/python/docker-compose.yml build
+# docker-compose --file environments/python/docker-compose.yml up
+docker-compose --file environments/python/docker-compose.yml run --rm python-ctr
